e2326f66f4508e3c96163ffea94ce02c824817bb
opilane <opilane@HP-CZC6297B98>
2020-12-08 12:21:40 +0200

    6

--- Adventure_Time/Adventure_Time/MainPage.xaml.cs
+++ Adventure_Time/Adventure_Time/MainPage.xaml.cs
@@ -27,42 +27,54 @@ namespace Adventure_Time
             list.Add(new MasterMenuItems()
             {
                 Text = "AdventureTime",
-                Detail = "",
+                Detail = "Фин Обладает строгим моральным кодексом, Финн поклялся, " +
+                "что поможет любому, кто в этом нуждается, и часто совершает героические " +
+                "подвиги со своим лучим дрцгом Джеком",
                 ImagePath = "Fin.jpg",
                 TargetPage = typeof(Characters.FinntheKid)
             });
             list.Add(new MasterMenuItems()
             {
                 Text = "AdventureTime",
-                Detail = "",
+                Detail = "Джейк это волшебный пес, который помагает своему лучшему другу Фину в приключениях",
                 ImagePath = "Jake.jpg",
                 TargetPage = typeof(Characters.Jake)
             });
             list.Add(new MasterMenuItems()
             {
                 Text = "AdventureTime",
-                Detail = "",
+                Detail = "БиМО — это игровая видео-приставка Финна и Джейка, " +
+                "а также портативная розетка, плеер, камера, будильник, тостер, " +
+                "фонарик, стробоскоп, программа для редактирования видео, " +
+                "видео-плеер,зарядное устройство, магнитофон и просто друг и сосед.",
                 ImagePath = "BMO.jpg",
                 TargetPage = typeof(Characters.BMO)
             });
             list.Add(new MasterMenuItems()
             {
                 Text = "AdventureTime",
-                Detail = "",
+                Detail = "Жвачка, как правило, очень добрая и сострадательная, " +
+                "у нее сильное чувство справедливости, хотя она показывает темную сторону, " +
+                "когда ее провоцируют. Она очень умна и отвечает за создание Конфетных Людей, " +
+                "а также за создание различных устройств, включая миниатюрную машину времени. ",
                 ImagePath = "bubblegum.jpg",
                 TargetPage = typeof(Characters.PrincessGum)
             });
             list.Add(new MasterMenuItems()
             {
                 Text = "AdventureTime",
-                Detail = "",
+                Detail = "Она вампир но в отличие от традиционного вампира, Марселин не нужно пить кровь, чтобы выжить; скорее она просто пьёт красный цвет. " +
+                "Марселин также музыкантша, что играет на электронной бас-гитаре. " +
+                "Она сделала из своего боевого топора, что когда-то был семейной реликвией.",
                 ImagePath = "marceline.jpg",
                 TargetPage = typeof(Characters.Marceline)
             });
             list.Add(new MasterMenuItems()
             {
                 Text = "AdventureTime",
-                Detail = "",
+                Detail = "Ледяной Король — пожилой мужчина низкого роста с голубым цветом кожи, " +
+                "крючковатыми пальцами на руках, тонкими пальцами на ногах, " +
+                "острыми зубами и носом, наподобие гоблинского.",
                 ImagePath = "IceKing.jpg",
                 TargetPage = typeof(Characters.IceKing)
             });
